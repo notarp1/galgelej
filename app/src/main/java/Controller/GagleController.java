686cbdb9ee0f5be74094d809b7cc77e3ba8bbbcb
christian <ChristiaNotarmaso@gmail.com>
2020-09-21 19:00:17 +0200

    Galgespil

--- app/src/main/java/Controller/GagleController.java
+++ app/src/main/java/Controller/GagleController.java
@@ -1,10 +1,17 @@
 package Controller;
 
+import android.widget.TextView;
+
+import com.example.galgelej.R;
+
+import java.util.ArrayList;
+
 import DTO.Galgelogik;
 
 public class GagleController {
 
     Galgelogik spil;
+    private ArrayList<String> brugteBogstaver;
 
     public GagleController(){
         spil = new Galgelogik();
@@ -15,10 +22,17 @@ public class GagleController {
         spil.nulstil();
     }
 
-    public String guess(String character){
+    public String guess(String character, TextView text){
         spil.gaetBogstav(character);
+        brugteBogstaver = getBrugteBogstaver();
+        String usedChars = " ";
+        for(int i = 0; i<brugteBogstaver.size(); i++){
+            usedChars += brugteBogstaver.get(i) + "-";
+        }
+        text.setText(usedChars);
+
         if(erSidsteBogstavKorrekt()){
-            return "Bogstavet var korrekt: " + character + "'";
+            return "Bogstavet var korrekt: '" + character + "'";
         } else {
             return "Bogstavet var forkert: '" + character + "'";
         }
@@ -27,38 +41,60 @@ public class GagleController {
     public String getSynligtOrd(){
         return spil.getSynligtOrd();
     }
-    public String getAntalForkerteBogstaver(){
+    public int getAntalForkerteBogstaver(){
 
         switch (spil.getAntalForkerteBogstaver()){
-            case 0:
-                return  "galge.png";
-
             case 1:
-                return "forkert1.png";
-                break;
+                return R.drawable.forkert1;
             case 2:
-                return "forkert2.png";
-                break;
+                return R.drawable.forkert2;
             case 3:
-                return "forkert3.png";
-                break;
+                return R.drawable.forkert3;
             case 4:
-                return "forkert4.png";
-                break;
+                return R.drawable.forkert4;
             case 5:
-                return "forkert5.png";
-                break;
+                return R.drawable.forkert5;
             case 6:
-                return "forkert6.png";
-                break;
+                return R.drawable.forkert6;
             default:
-                return "galge.png";
+                return R.drawable.galge;
         }
 
     }
+
     public boolean erSidsteBogstavKorrekt(){
         return spil.erSidsteBogstavKorrekt();
     }
 
+    public int spilStatus(){
+        if(erSpilletSlut()){
+            if(erSpilletVundet()){
+                return 0;
+            } else {
+                return 1;}
+        }else if(erSidsteBogstavKorrekt()) {
+            return 2;
+        } else return 3;
+    }
+
+    public boolean erSpilletSlut(){
+        return spil.erSpilletSlut();
+    }
+
+    public boolean erSpilletVundet() {
+        return spil.erSpilletVundet();
+    }
+
+    public boolean erSpilletTabt() {
+        return spil.erSpilletTabt();
+    }
+
+    public ArrayList<String> getBrugteBogstaver() {
+        return spil.getBrugteBogstaver();
+    }
+
+    public String getOrdet(){
+        return spil.getOrdet();
+    }
 
 }
--- app/src/main/java/com/example/galgelej/GagleSpil.java
+++ app/src/main/java/com/example/galgelej/GagleSpil.java
@@ -1,13 +1,23 @@
 package com.example.galgelej;
 
+import android.annotation.SuppressLint;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
 
 import Controller.GagleController;
 
@@ -18,6 +28,7 @@ public class GagleSpil extends AppCompatActivity {
     GagleController spil;
     TextView word;
     TextView status;
+    TextView guessesWords;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,26 +40,56 @@ public class GagleSpil extends AppCompatActivity {
         charInput = findViewById(R.id.inputString);
         bGuess = findViewById(R.id.buttonGuess);
         word = findViewById(R.id.textWord);
+        guessesWords = findViewById(R.id.textGuesses);
         status = findViewById(R.id.textStatus);
 
         spil.nulstil();
         word.setText(spil.getSynligtOrd());
         status.setText("Indtast dit første bogstav");
 
-
         bGuess.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 String input = charInput.getText().toString();
-                status.setText(spil.guess(input));
+                status.setText(spil.guess(input, guessesWords));
                 word.setText(spil.getSynligtOrd());
-                System.out.println(spil.getAntalForkerteBogstaver());
+
+                int wrongAnswer = spil.getAntalForkerteBogstaver();
+                ((ImageView)findViewById(R.id.imageView)).setImageResource(wrongAnswer);
+
+                String toast = "";
+                switch (spil.spilStatus()){
+                    case 0:
+                        toast = "Tilykke du har vundet";
+                        nulstilSpil();
+                        break;
+                    case 1:
+                        toast = "Du har tabt. Ordet var: " + spil.getOrdet();
+                        nulstilSpil();
+                        break;
+                    case 2:
+                        toast = "Rigtigt Svar!";
+                        break;
+                    case 3:
+                        toast = "Forkert svar";
+                        break;
+                }
+                Toast.makeText(GagleSpil.this, toast, Toast.LENGTH_LONG).show();
+
             }
         });
 
 
 
 
+    }
+
+    private void nulstilSpil() {
+        spil.nulstil();
+        guessesWords.setText("");
+        status.setText("Indtast dit første bogstav");
+        ((ImageView) findViewById(R.id.imageView)).setImageResource(R.drawable.galge);
+        word.setText(spil.getSynligtOrd());
     }
 }
